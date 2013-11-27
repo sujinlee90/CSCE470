fc095affd9885355a7b52c44a04c9f49572eb169
sujinlee90 <sujinlee90@gmail.com>
2013-11-26 17:24:09 -0800

    new version

--- cluster_copy.py
+++ cluster_copy.py
@@ -5,8 +5,8 @@ import math
 import json
 import os
 import collections
-from operator import itemgetter
-	
+import operator
+
 def read_data(filename):
     """
     Used to read all tweets from the json file.
@@ -52,14 +52,14 @@ class Recommender():
 	#Count the number of different TV shows that were cited on the list of tweets
 	#from a certain user. The parameter passed to the function is a vector with the
 	#number of tweets for each TV show.
-	def count_dif_tvshows(list):
+	def count_dif_tvshows(self, list):
             n_tvshows = 0
 		#count is the number of tweets about a certain TV show
-		for count in list:
+            for count in list:
 		    #if count > 0, it means that the user mentioned the TV show at least once
 		    #if not, it means that there is no tweet about that TV show posted by this user
-		    if count > 0:
-			n_tvshows += 1
+		if count > 0:
+		    n_tvshows += 1
 	    return n_tvshows
 	
 	def index_tweets(self):
@@ -77,7 +77,7 @@ class Recommender():
 		#Calculating the index of the TV show
 		index += 1
 			
-		process_tweets(tweets, index)
+		self.process_tweets(tweets, index)
 		
 	    #Creating a vector (Python list) to store the number of
 	    #different TV shows each user mentioned
@@ -85,7 +85,7 @@ class Recommender():
 	    i = 0
 	    #For each user, calculate the number of different TV shows his/her tweets mention
 	    for user in self.users:
-		number_dif_tvshows[i] = count_dif_tvshows(self.users[user])
+		number_dif_tvshows[i] = self.count_dif_tvshows(self.users[user])
 		i += 1
 		
 	    #Creating a vector (Python list) to store the number of
@@ -94,7 +94,7 @@ class Recommender():
 	    #that 100 users twitted about 3 different TV shows
 	    number_users_n_dif_tvshows = [0]*len(self.list_tvshows)	
 	    for user in self.users:
-		number_users_n_dif_tvshows[count_dif_tvshows(self.users[user])] += 1
+		number_users_n_dif_tvshows[self.count_dif_tvshows(self.users[user])] += 1
 
 	    print 'Number of users: ', len(self.users)
 	    print number_users_n_dif_tvshows
@@ -111,11 +111,12 @@ class Recommender():
             """
             #find index for tv show the user searches
             tv_show_index = self.list_tvshows.index(tv_show)
+            #print  "tv_show_index = ", tv_show_index, len(self.users), self.users[tv_show_index]
 
             #find user who mentioned the tv show and mentioned more than or equal to two tv shows
             for user in self.users:
-                if self.users[tv_show_index] != 0:
-                    if count_dif_tvshows(self.users[user]) > 1:
+                if user[tv_show_index] != 0:
+                    if self.count_dif_tvshows(self.users[user]) > 1:
                         for i in range(0, len(self.list_tvshows)):
                             self.sum_tvshows[i] = self.sum_tvshows[i] + self.users[user][i]
 
@@ -128,7 +129,7 @@ class Recommender():
             tvshow_index = self.list_tvshows.index(tv_show)
             # print 10 users who mentioned the tv show
             for i in range(0,10):
-                print 'User:', self.tweet[index][i]['user'], ', Text:', self.tweet[index][i]['text']
+                print 'User:', self.tweet[tvshow_index][i]['user'], ', Text:', self.tweet[tvshow_index][i]['text']
 
         def recommend_tvshows(self, tv_show):
             """
@@ -140,17 +141,16 @@ class Recommender():
                 dic_tvshows[self.list_tvshows[n]] = self.sum_tvshows[n]
 
             #sort tv shows by reverse order
-            sorted_tvshows = sorted(dic_tvshows.iteritems(), key = operator.itemgetter(1), reverse = True)
-
-            #save top 5 tv shows
-            top_5_tvshows = shorted_tvshows[:5]
+            sorted_tvshows = list(sorted(dic_tvshows, key = dic_tvshows.__getitem__, reverse = True))
             
             #find users who mentioned tv shows in ranking top 5 and print users' text
-            print_user_mentions_tvshow(top_5_tvshows.keys()[0]) #print users' mention for top 1
-            print_user_mentions_tvshow(top_5_tvshows.keys()[1]) #print users' mention for top 2
-            print_user_mentions_tvshow(top_5_tvshows.keys()[2]) #print users' mention for top 3
-            print_user_mentions_tvshow(top_5_tvshows.keys()[3]) #print users' mention for top 4
-            print_user_mentions_tvshow(top_5_tvshows.keys()[4]) #print users' mention for top 5
+            self.print_user_mentions_tvshow(sorted_tvshows[0]) #print users' mention for top 1
+            #self.print_user_mentions_tvshow(sorted_tvshows.keys()[1]) #print users' mention for top 2
+            #self.print_user_mentions_tvshow(sorted_tvshows.keys()[2]) #print users' mention for top 3
+            #self.print_user_mentions_tvshow(sorted_tvshows.keys()[3]) #print users' mention for top 4
+            #self.print_user_mentions_tvshow(sorted_tvshows.keys()[4]) #print users' mention for top 5
+
+            print sorted(dic_tvshows.iteritems(), key = operator.itemgetter(1), reverse = True)[:5]
             
 		
 if __name__ == "__main__":
